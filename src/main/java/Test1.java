43d6411fdccc6f44c18ea30630574e11bb336361
yuza <yuza.plan@gmail.com>
2019-05-25 18:16:20 +0900

    second commit

--- src/main/java/Test1.java
+++ src/main/java/Test1.java
@@ -2,10 +2,10 @@ public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println();
-		System.out.println(getSum(1, 2));
+		System.out.println(getSum(1, 2, 3));
 	}
 
-	private static int getSum(int num1, int num2) {
-		return num1 + num2;
+	private static int getSum(int num1, int num2, int num3) {
+		return num1 + num2 + num3;
 	}
 }
